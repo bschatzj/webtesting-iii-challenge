3b2d8ea880eb39157fded315fd773cec876a55de
Brendan Schatz <brendanschatz97@gmail.com>
2019-11-11 23:48:11 -0500

    mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,46 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import Controls from './Controls';
+
+test('control render correct' , () => {
+    expect(render(<Controls/>)).toMatchSnapshot();
+});
+
+
+test('unLock Gate', () => {
+    const toggleLockMock = jest.fn();
+    const {getByText} = render(<Controls toggleLocked={toggleLockMock} locked={true}/>)
+
+    const unlockBtn = getByText(/Unlock Gate/i)
+    fireEvent.click(unlockBtn);
+    expect(toggleLockMock).not.toHaveBeenCalled();
+})
+
+test('Lock Gate', () => {
+    const toggleunLockMock = jest.fn();
+    const {getByText} = render(<Controls toggleLocked={toggleunLockMock} locked={false}/>)
+
+    const lockBtn = getByText(/Lock Gate/i)
+    fireEvent.click(lockBtn);
+    expect(toggleunLockMock).not.toHaveBeenCalled();
+})
+
+
+test('open Gate', () => {
+    const toggleOpenMock = jest.fn();
+    const {getByText} = render(<Controls toggleClosed={toggleOpenMock} closed={true}/>)
+
+    const openBtn = getByText(/open/i)
+    fireEvent.click(openBtn);
+    expect(toggleOpenMock).toHaveBeenCalled();
+})
+
+test('close Gate', () => {
+    const toggleCloseMock = jest.fn();
+    const {getByText} = render(<Controls toggleClosed={toggleCloseMock} closed={false}/>)
+
+    const closeBtn = getByText(/close/i)
+    fireEvent.click(closeBtn);
+    expect(toggleCloseMock).toHaveBeenCalled();
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
-// Test away!
+import React from 'react';
+import {render, getByDisplayValue} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+import Display from './Display';
+
+test('control render correct' , () => {
+    expect(render(<Display/>)).toMatchSnapshot();
+});
+
+
+test('Uses "red-led" if locked.', () => {
+    const { queryByText } = render(<Display locked={true} />);
+    const locked = queryByText(/locked/i);
+    expect(locked).toHaveClass('red-led');
+});
+
+test('Uses "green-led" if locked.', () => {
+    const { queryByText } = render(<Display locked={false} />);
+    const unlocked = queryByText(/locked/i);
+    expect(unlocked).toHaveClass('green-led');
+});
